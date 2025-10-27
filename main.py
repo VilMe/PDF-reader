8f5e6ad72382e369487de5bbbee8a8c5b8e5c57a
VilMe <vilma.medrano00@gmail.com>
2025-10-26 22:52:42 -0500

    begin to definnne extract text from pdf function

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from collections import Counter
 from PyPDF2 import PdfReader
 
 
+def extract_text_from_pdf(pdf_file:str) -> list[str]:
+    with open(pdf_file, 'rb') as pdf:
+        reader = PdfReader(pdf, strict=False)
