399a441f31d7396a2fb990bb5cc02d694bae146a
VilMe <vilma.medrano00@gmail.com>
2025-10-26 22:55:23 -0500

    add print statements for output header

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from PyPDF2 import PdfReader
 def extract_text_from_pdf(pdf_file:str) -> list[str]:
     with open(pdf_file, 'rb') as pdf:
         reader = PdfReader(pdf, strict=False)
+
+
+
+        print('Pages:', len(reader.pages))
+        print('-' * 10) # Divider
+
+        
